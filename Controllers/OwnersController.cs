5c60e7ba316ca0de07447d9bdc774373708d3793
Zach McWhirter <zmac7811@gmail.com>
2020-09-22 15:27:07 -0500

    Get user specific info for dogs

--- Controllers/OwnersController.cs
+++ Controllers/OwnersController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using DogGo.Models;
 using DogGo.Models.ViewModels;
 using DogGo.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -156,39 +159,41 @@ namespace DogGo.Controllers
             }
         }
 
-    //  Login Method
+        //  Login Methods
 
-    //    public ActionResult Login()
-    //    {
-    //        return View();
-    //    }
+        //  GET
+            public ActionResult Login()
+        {
+            return View();
+        }
 
-    //    [HttpPost]
-    //    public async Task<ActionResult> Login(LoginViewModel viewModel)
-    //    {
-    //        Owner owner = _ownerRepo.GetOwnerByEmail(viewModel.Email);
+        //  POST
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel viewModel)
+        {
+            Owner owner = _ownerRepo.GetOwnerByEmail(viewModel.Email);
 
-    //        if (owner == null)
-    //        {
-    //            return Unauthorized();
-    //        }
+            if (owner == null)
+            {
+                return Unauthorized();
+            }
 
-    //        var claims = new List<Claim>
-    //{
-    //    new Claim(ClaimTypes.NameIdentifier, owner.Id.ToString()),
-    //    new Claim(ClaimTypes.Email, owner.Email),
-    //    new Claim(ClaimTypes.Role, "DogOwner"),
-    //};
+            var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, owner.Id.ToString()),
+            new Claim(ClaimTypes.Email, owner.Email),
+            new Claim(ClaimTypes.Role, "DogOwner"),
+        };
 
-    //        var claimsIdentity = new ClaimsIdentity(
-    //            claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsIdentity = new ClaimsIdentity(
+                claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-    //        await HttpContext.SignInAsync(
-    //            CookieAuthenticationDefaults.AuthenticationScheme,
-    //            new ClaimsPrincipal(claimsIdentity));
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity));
 
-    //        return RedirectToAction("Index", "Dogs");
-    //    }
+            return RedirectToAction("Index", "Dogs");
+        }
 
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DogGo.Repositories;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,7 +31,9 @@ namespace DogGo
             services.AddTransient<IDogRepository, DogRepository>();
             services.AddTransient<INeighborhoodRepository, NeighborhoodRepository>();
             services.AddTransient<IWalkRepository, WalkRepository>();
-            //Auth goes here
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => options.LoginPath = "/Owners/LogIn");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +54,7 @@ namespace DogGo
 
             app.UseRouting();
 
-            //app.UseAuthentication();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
